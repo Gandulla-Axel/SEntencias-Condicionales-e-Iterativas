24a434612cd4854bebbdedacae8c2fb53b131d7c
axel Name <gandullaaxel727.gmail.com>
2022-07-15 16:10:38 -0300

    iterativas1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        String foo = "foo";
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(foo);
+        }
+
+    }
+}
